ac7439f7175a1e867b2d3efbc838bfa9857e3a6e
chien06111998 <chien1998qb@gmail.com>
2021-04-12 18:50:50 +0700

    nộp lại bài tập

--- module1/Nhap_Mon_Lap_Trinh/ThucHanh/6. Tổng quan Javascript/thực hành/demo.js
+++
@@ -1,5 +0,0 @@
-var demo = document.getElementById("demo2");
-demo = document.addEventListener("click", click_function);
-function click_function() {
-    alert("hé lồ bấy bì");
-}
---
+++ module1/Nhap_Mon_Lap_Trinh/ThucHanh/6.tong_quan_javascript/thuc_hanh/demo.js
@@ -0,0 +1,5 @@
+// var demo2 = document.getElementById("demo2");
+// demo2 = document.addEventListener("click", click_function);
+function click_function() {
+    alert("hé lồ bấy bì");
+}
